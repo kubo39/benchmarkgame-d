c65bff18cbbec06445a9574c912e17689e3cd919
Hiroki Noda <kubo39@gmail.com>
2019-06-20 08:37:43 +0900

    Improve

--- source/threadrng.d
+++ source/threadrng.d
@@ -1,45 +1,38 @@
 // ldc2 -O3 threadrng.d
 
-import std.stdio;
+import core.stdc.stdio;
 import std.concurrency;
+import std.conv : parse;
 
 void start(int nTasks, int token)
 {
-    // 最後に送るTidは必ずメインスレッド
-    Tid tid = thisTid;
-
-    // id: nのスレッドはid: n+1のスレッドに送る
-    foreach (id; 1 .. nTasks) {
-        Tid currTid = spawn(&roundtrip, tid, thisTid, id, nTasks);
-        // 次に生成するスレッドは現在のスレッドIDに送信する
-        tid = currTid;
-    }
-
     // メインスレッドは最後のIDを持つスレッドに送信
-    Tid firstTid = spawn(&roundtrip, tid, thisTid, nTasks, nTasks);
+    Tid tid = spawn(&roundtrip, thisTid, 1 /* id */, nTasks);
+    // id: nのスレッドはid: n+1のスレッドに送る
+    foreach (id; 2 .. (nTasks + 1))
+        // 次に生成するスレッドはひとつ前のスレッドIDに送信する
+        tid = spawn(&roundtrip, tid, id, nTasks);
 
-    while (token > 0) {
-        send(firstTid, token);
+    while (token > 0)
+    {
+        tid.send(token);
         token = receiveOnly!int();
     }
 }
 
-void roundtrip(Tid tid, Tid ownerTid, int id, int nTasks)
+void roundtrip(Tid tid, int id, int nTasks)
 {
-    int token;
-    while (true) {
-        receive(
-                (int _token) {
-                    token = _token;
-                });
-
-        if (token == 1) {
-            writefln("id: %d", id);
-            send(ownerTid, token - 1);
+    while (true)
+    {
+        auto token = receiveOnly!int;
+        if (token == 1)
+        {
+            printf("id: %d\n", id);
+            ownerTid.send(token - 1);
             break;
         }
         else
-            send(tid, token - 1);
+            tid.send(token - 1);
         if (token <= nTasks)
             break;
     }
@@ -47,7 +40,7 @@ void roundtrip(Tid tid, Tid ownerTid, int id, int nTasks)
 
 void main(string[] args)
 {
-    auto token = 1000;
-    auto nTasks = 503;
+    auto token = args.length > 1 ? parse!int(args[1]) : 1000;
+    auto nTasks = args.length > 2 ? parse!int(args[2]) : 503;
     start(nTasks, token);
 }
